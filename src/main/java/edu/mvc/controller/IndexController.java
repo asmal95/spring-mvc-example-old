85acb4e34337c4af1793bc9c84b53f80a84c4a13
Andrew <asmal95@mail.ru>
2018-02-23 23:35:17 +0400

    Added message from server to the view.

--- src/main/java/edu/mvc/controller/IndexController.java
+++ src/main/java/edu/mvc/controller/IndexController.java
@@ -1,13 +1,15 @@
 package edu.mvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 
     @GetMapping
-    public String index() {
+    public String index(ModelMap model) {
+        model.addAttribute("message", "Hello from server!");
         return "index";
     }
 }
